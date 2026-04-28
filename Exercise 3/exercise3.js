c7cfe1e53f2807c42c5fafbdba988875a5f3a797
LiiQLiiQato77 <buruuj95@gmail.com>
2026-04-28 11:27:55 +0300

    added exercise 3 js

---
+++ Exercise 3/exercise3.js
@@ -0,0 +1,12 @@
+let isSunny = true;
+let isWeekend = true;
+
+// and &&
+console.log(isSunny && isWeekend);
+
+// or |
+console.log(isSunny || isWeekend)
+
+// invert 
+console.log(!isSunny)
+console.log(!isWeekend)
