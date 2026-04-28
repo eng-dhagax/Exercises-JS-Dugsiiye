7374ad4186da3f8845d1d0c04a7dc7cf1627b6aa
LiiQLiiQato77 <buruuj95@gmail.com>
2026-04-28 11:17:32 +0300

    added exercise2js

---
+++ Exercise 2/exercise2.js
@@ -0,0 +1,26 @@
+let a = 15;
+let b = "15";
+
+// equal to
+console.log(a == b);
+
+// not equal to
+console.log(a != b)
+
+// strictly equal to 
+console.log(a === b)
+
+// strictly not equal to 
+console.log(a !== b)
+
+// greater than 
+console.log(a > b)
+
+// less than 
+console.log(a > b)
+
+// greater than or equal to
+console.log(a >= b)
+
+// less than or equal to
+console.log(a <= b)
