3cee9f89734826368a81541b7bef94afbeaa384a
LiiQLiiQato77 <buruuj95@gmail.com>
2026-04-30 18:01:01 +0300

    complated exercise 4

---
+++ Exercise 4/exercise4.js
@@ -0,0 +1,19 @@
+function add (b,d) {
+    return b+d;
+}
+
+console.log(add(20,30))
+
+console.log(add(7,8))
+
+let result = add(15,10)
+console.log(result / 2)
+
+// Function Expression
+const addExpression = function(b,d) {
+    return  b+d;
+}
+
+console.log(addExpression(4,3))
+console.log(addExpression(7, 10))
+console.log(addExpression(10, 60))
