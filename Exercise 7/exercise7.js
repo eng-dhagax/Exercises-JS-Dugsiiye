3a06b4515e9275410414dcad0b3e3d9fe10105a1
LiiQLiiQato77 <buruuj95@gmail.com>
2026-05-18 21:53:19 +0300

    Completed Exercise 7

---
+++ Exercise 7/exercise7.js
@@ -0,0 +1,12 @@
+let colors = ["Navy", "Purple", "Pink"]
+console.log(colors)
+
+// Push 
+console.log("After Push")
+colors.push("Peach")
+console.log(colors)
+
+// Shift 
+console.log("After Shift")
+colors.shift()
+console.log(colors)
