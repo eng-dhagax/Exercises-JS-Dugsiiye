59892ea1373e6c87bba35124e60556199c5e9554
LiiQLiiQato77 <buruuj95@gmail.com>
2026-06-12 20:32:25 +0300

    submited exercise 9

---
+++ Exercise 9/exercise9.js
@@ -0,0 +1,16 @@
+// Exercise 9
+let car = {
+    make : "Toyota",
+    model : "RAV4",
+    year : 2024,
+
+    start : function() {
+        console.log("The car has started")
+    }
+}
+
+console.log(car.make)
+console.log(car.model)
+console.log(car.year)
+
+console.log(car.start())
