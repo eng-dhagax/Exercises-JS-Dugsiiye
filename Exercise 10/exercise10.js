0ef174ce95b559fecd8df1b3279a7d3a929ec403
LiiQLiiQato77 <buruuj95@gmail.com>
2026-06-12 21:42:27 +0300

    completed exercise 10

---
+++ Exercise 10/exercise10.js
@@ -0,0 +1,24 @@
+// Exercise #10
+let library = [
+    {
+        title : "Al-Muqaddimah",
+        author : "Ibn Khaldun",
+        year :  1377
+    },
+
+    {
+
+        title : " Taariikhda Soomaaliya (Dalkii Filka Weynaa ee Punt)",
+        author : "Muxamed Ibraahim Muxamed “Liiq-Liiqato”",
+        title : 2000
+    }, 
+
+    {
+        title : "The Prince",
+        author : "Niccolò Machiavelli",
+        year : 1532
+    }
+]
+
+console.log(library[0].title)
+console.log(library[1].author)
