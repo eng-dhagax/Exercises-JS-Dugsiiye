6711966c5c3c7ebd35e4be360d0b8ad80e909208
LiiQLiiQato77 <buruuj95@gmail.com>
2026-06-22 19:02:51 +0300

    Implement spread and rest operator exercise 26

---
+++ Exercise 25/exercise25.js
@@ -0,0 +1,18 @@
+// Exercise 25
+console.log("Spread Operator")
+let numbers = [1,2,3];
+
+let newNumbers = [...numbers, 4,5,6];
+
+console.log(newNumbers);
+
+console.log("---");
+
+console.log("Rest Operator")
+function multiply (...number) {
+
+    return number.reduce((total,num) => total * num, 1);
+
+};
+
+console.log(multiply(2,2,509))
